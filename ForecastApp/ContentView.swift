5ecb96d814c9f54bb6c3d60c5846546e60832ac3
julio.collado <julio.collado@overactive.com>
2021-07-02 18:46:46 -0300

    Initial Commit

---
+++ ForecastApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ForecastApp
+//
+//  Created by Julio Collado on 2/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ForecastApp/ForecastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastAppApp.swift
+//  ForecastApp
+//
+//  Created by Julio Collado on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ForecastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
